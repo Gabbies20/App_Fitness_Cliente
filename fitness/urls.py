7ef26b01c1df1ec889e576e277666530f3eda454
Gabbies20 <gabriela.pinzon.ochoa.al@iespoligonosur.org>
2024-01-24 14:46:32 +0100

    Busqueda simple terminada

--- fitness/urls.py
+++ fitness/urls.py
@@ -4,6 +4,6 @@ from .import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('lista_ejercicios',views.ejercicios_lista_api,name='lista'),
-    path('busqueda',views.busqueda_simple,name='busqueda_simple'),
+    path('busqueda',views.ejercicio_busqueda_simple,name='busqueda_simple'),
     
 ]
--- fitness/views.py
+++ fitness/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render,redirect
-from django.views.defaults import page_not_found
-
-from django.contrib import messages
-from django.db.models import Q,F
+from django.shortcuts import render,redirect,HttpResponse
 from .forms import *
-from datetime import datetime
-from django.contrib.auth.models import Group
-from django.contrib.auth import login
+# Create your views here.
+#Vistas API
+
+
 import requests
-from django.core import serializers
+#import environ
+import os
+from pathlib import Path
+
+
+#.ENV:
+#BASE_DIR = Path(__file__).resolve().parent.parent
+#env = environ.Env()
+#environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Create your views here.
@@ -16,13 +22,32 @@ def index(request):
   
     return render(request, 'fitness/index2.html')
 
+def crear_cabecera():
+    return {'Authorization': 'Bearer CLSw8ZZaTtNb7oKIAb9ttWgN8EZlPE'}
+
 
 def ejercicios_lista_api(request):
     #Obtenemos los ejercicios.
-    headers = {'Authorization':'Bearer 8LzrEEahgeoE6wsFHKKVRXZuQ3HFfy'}
+    headers = {'Authorization':'Bearer CLSw8ZZaTtNb7oKIAb9ttWgN8EZlPE'}
     response = requests.get('http://127.0.0.1:8000/api/v1/ejercicios',headers=headers)
     #Transformamos la respuesta de json.
     ejercicios = response.json()
     return render(request, 'fitness/lista_api.html',{'ejercicios_mostrar':ejercicios})
 
 #Debi acceder a la URL y no me muestra nada, de forma interna me esta dando un error 400. Ahora al crear 'headers' con su respectivo token me muestra ya los libros.
+def ejercicio_busqueda_simple(request):
+    formulario = BusquedaEjercicioForm(request.GET)
+    
+    if formulario.is_valid():
+        headers = crear_cabecera()
+        response = requests.get(
+            'http://127.0.0.1:8000/api/v1/ejercicios/busqueda_simple',
+            headers = headers,
+            params = formulario.cleaned_data
+        )
+        ejercicios = response.json()
+        return render(request,'fitness/ejercicio/lista_mejorada.html',{'ejercicios_mostrar':ejercicios})
+    if("HTTP_REFERER" in request.META):
+        return redirect(request.META["HTTP_REFERER"])
+    else:
+        return redirect("index")
